7fd40fa9dc0b08da82429ef4bd2dd9c9b6b24f5d
iameffat <53122076+iameffat@users.noreply.github.com>
2024-05-07 10:55:58 +0600

    Update palo.js

--- palo.js
+++ palo.js
@@ -21,7 +21,8 @@
         });
 
         htmlContent += "<a class='prev' onclick='plusSlides(-1)'>&#10094;</a><a class='next' onclick='plusSlides(1)'>&#10095;</a>";
-        htmlContent += "</div><script>var slideIndex = 1;showSlides(slideIndex);function plusSlides(n) {showSlides(slideIndex += n);}function showSlides(n) {var i;var slides = document.getElementsByClassName('slides');if (n > slides.length) {slideIndex = 1} if (n < 1) {slideIndex = slides.length} for (i = 0; i < slides.length; i++) {slides[i].style.display = 'none';} slides[slideIndex-1].style.display = 'block';}</script></body></html>";
+        htmlContent += "<script>var slideIndex = 1;showSlides(slideIndex);function plusSlides(n) {showSlides(slideIndex += n);}function showSlides(n) {var i;var slides = document.getElementsByClassName('slides');if (n > slides.length) {slideIndex = 1} if (n < 1) {slideIndex = slides.length} for (i = 0; i < slides.length; i++) {slides[i].style.display = 'none';} slides[slideIndex-1].style.display = 'block';}</script>";
+        htmlContent += "</div></body></html>";
 
         var blob = new Blob([htmlContent], {type: "text/html"});
         var link = document.createElement("a");
@@ -54,19 +55,4 @@
         addDownloadButton();
     });
 
-    // Define global functions
-    window.plusSlides = function(n) {
-        showSlides(slideIndex += n);
-    }
-
-    window.showSlides = function(n) {
-        var i;
-        var slides = document.getElementsByClassName('slides');
-        if (n > slides.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = slides.length }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-        slides[slideIndex-1].style.display = 'block';
-    }
 })();
